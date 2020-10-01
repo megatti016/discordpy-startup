d1aebc0993a8b847ed2570769bc160bc8fda4760
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 03:23:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import os
+import discord
 import traceback
 
 bot = commands.Bot(command_prefix='/')
@@ -16,11 +17,4 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-async def on_member_join(member):
-   CHANNEL_ID = 699621884643377194
-    channel = bot.get_channel(CHANNEL_ID)
-    infomation_ch = bot.get_channel(699621884643377194)
-    zikosyoukai_ch = bot.get_channel(699621884643377194)
-    await channel.send(str(member.mention)+'音MAD作者プリコネサーバー へようこそ！\n'+str(infomation_ch.mention)+ 'をご一読ください\n' + str(zikosyoukai_ch.mention)+ 'で音MAD作者とわかるようなURLと軽い自己紹介をよろしくお願いします\n※このチャンネルの通知をオフにすることを推奨します')
-      
 bot.run(token)
