bce0a1c7412dd53bc3a7a91eb4e14536289812bf
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 03:20:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,14 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@bot.event
+async def on_member_join(member):
+#招待通知
+   CHANNEL_ID = 699621884643377194
+    #710726901051818068
+    channel = bot.get_channel(CHANNEL_ID)
+    infomation_ch = bot.get_channel(699621884643377194)
+    zikosyoukai_ch = bot.get_channel(699621884643377194)
+    await channel.send(str(member.mention)+'音MAD作者プリコネサーバー へようこそ！\n'+str(infomation_ch.mention)+ 'をご一読ください\n' + str(zikosyoukai_ch.mention)+ 'で音MAD作者とわかるようなURLと軽い自己紹介をよろしくお願いします\n※このチャンネルの通知をオフにすることを推奨します')
+      
 bot.run(token)
