4e60b8b0c1a47af57be5ff27d55651b41f3bbb4d
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 03:27:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,8 +22,5 @@ CHANNEL_ID = '699621884643377194'
     channel = bot.get_channel(CHANNEL_ID)
     infomation_ch = bot.get_channel('699621884643377194')
     zikosyoukai_ch = bot.get_channel('699621884643377194')
-    await channel.send(str(member.mention)+'音MAD作者プリコネサーバー へようこそ！\n'+str(infomation_ch.mention)+ 'をご一読ください\n' + str(zikosyoukai_ch.mention)+ 'で音MAD作者とわかるようなURLと軽い自己紹介をよろしくお願いします\n※このチャンネルの通知をオフにすることを推奨します')
-  
-
-
+    
 bot.run(token)
