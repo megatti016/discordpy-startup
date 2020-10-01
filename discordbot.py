1a0f8b846e97b4b844cd0ea5fedc82b6358b108c
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 03:10:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,30 @@
 from discord.ext import commands
+import discord
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 接続に必要なオブジェクトを生成
+CLIENT = discord.Client()
 
-@bot.event
+@bot.command()
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
+@bot.event
+async def on_member_join(member):
+#招待通知
+   CHANNEL_ID = 699621884643377194
+    #710726901051818068
+    channel = bot.get_channel(CHANNEL_ID)
+    infomation_ch = bot.get_channel(699621884643377194)
+    zikosyoukai_ch = bot.get_channel(699621884643377194)
+    await channel.send(str(member.mention)+'音MAD作者プリコネサーバー へようこそ！\n'+str(infomation_ch.mention)+ 'をご一読ください\n' + str(zikosyoukai_ch.mention)+ 'で音MAD作者とわかるようなURLと軽い自己紹介をよろしくお願いします\n※このチャンネルの通知をオフにすることを推奨します')
+  
+# Botの起動とDiscordサーバーへの接続
 bot.run(token)
+
