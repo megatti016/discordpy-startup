dad13db27735614230d9a78eb99f22ea441c67b3
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 04:20:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,11 +7,11 @@ bot = commands.Bot(command_prefix='/')
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.command()
+@bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
