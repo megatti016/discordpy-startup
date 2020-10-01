8ba50319b9ae578b291d592cc8cdf30ffd96e625
megatti016 <67099155+megatti016@users.noreply.github.com>
2020-10-02 03:22:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,18 +6,16 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@bot.event
 async def on_member_join(member):
    CHANNEL_ID = 699621884643377194
     channel = bot.get_channel(CHANNEL_ID)
